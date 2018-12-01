80fbc960a41ef809c022866e8209e0aaf435adb6
Christian <christian.frommert@gmail.com>
2018-12-01 20:30:34 +0100

    Initial Commit

---
+++ database/migrations/2018_11_29_200344_create_log_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLogTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('goods_id')->references('id')->on('goods');
+            $table->integer('user_id')->references('id')->on('users');
+            $table->string('action');
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logs');
+    }
+}
